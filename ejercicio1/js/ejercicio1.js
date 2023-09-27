e572ea6587fabb8a8b252730799cd2b9092853b5
Sergioig37 <sergioig37@educastur.es>
2023-09-27 10:01:26 +0200

    Ejercicio 1 acabado

---
+++ ejercicio1/js/ejercicio1.js
@@ -0,0 +1,43 @@
+var numero = prompt("Introduce un numero");
+
+function esEntero(cadena){
+
+    var esEntero = true;
+
+   if(parseInt(cadena)){
+        return esEntero;
+   }
+   else{
+        esEntero = false;
+        return esEntero;
+   }
+}
+
+function esPositivo(numero){
+
+    var esPositivo = true;
+
+    if(numero>0){
+        return esPositivo;
+    }
+    else{
+        esPositivo = false;
+        return esPositivo;
+    }
+}
+
+if((esPositivo(numero)==false)||(esEntero(numero)==false)){
+   var numero= prompt("El número introducido no es válido, prueba de nuevo");
+   while((esPositivo(numero)==false)||(esEntero(numero)==false)){
+        var numero= prompt("El número introducido no es válido, prueba de nuevo");
+   }
+}
+else if((esPositivo(numero)!=false)&&(esEntero(numero)!=false)){
+    var resultado = 0;
+    for(let i=0; i<=numero; i++){
+         resultado += i ;
+    }
+
+    alert(resultado);
+}
+
