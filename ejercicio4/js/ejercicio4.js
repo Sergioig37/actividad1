97a5d271f16e3552db8ab86bfcb0e0763b7c8dff
Sergioig37 <sergioig37@educastur.es>
2023-09-28 17:09:00 +0200

    Ejercicio 4 hecho

--- ejercicio4/js/ejercicio4.js
+++ ejercicio4/js/ejercicio4.js
@@ -1,33 +1,35 @@
-var operacion = prompt("Inserta el signo de la operación que deseas realizar");
 
-var operando1 = prompt("Inserta un número");
+function validarOperador(){
 
-var operando2 = prompt("Inserta otro número");
-
-function validarOperador(cadena){
+    var operacion = prompt("Inserta el signo de la operación que deseas realizar");
 
     var operadoresValidos = ["+", "*", "/", "-", "FIN"];
-    var valido = true;
-    var i=0;
-
-    while(i<operadoresValidos||valido==false){
-        if(i===cadena){
-            break;
-        }
-        else{
-            valido = false;
-        }
-        i++;
+    
+    var encontrado = false;
+    
+    for(var i=0; i<operadoresValidos.length; i++){
+                if(operadoresValidos[i]===operacion){
+                    encontrado = true;
+                    break;
+                }
     }
         
-    if(valido==false){
-        operacion = prompt("Inserte un signo de operación válido");
+    if(encontrado ===false){
+       
+        while(encontrado===false){
+            operacion = prompt("Inserta un signo de  operación válido");
+            for(var i=0; i<operadoresValidos.length; i++){
+                if(operadoresValidos[i]===operacion){
+                    encontrado = true;
+                    break;
+               }
+            }
+        }  
     }
-
-    return valido;
+    return operacion;
 }
 
-function divisionValida(operando1, operando2){
+function divisionValida( ){
 
     var divisorNull = false;
 
@@ -36,33 +38,66 @@ function divisionValida(operando1, operando2){
         divisorNull = true;
     }
     
-    return divisorNull;
+    return operando2;
 }
 
 
 function hacerOperacion(cadena, operando1, operando2){
 
-    validarOperador(cadena);
 
-    divisionValida(operando1, operando2);
+    if(cadena==="/"){
+        operando2 = divisionValida(operando1, operando2);
+    }
+    
+
+    var resultado;
 
     switch(cadena){
         case"+":
-         operando1 +operando2; break;
+          resultado = operando1 + operando2; 
+           alert(resultado);
+          break;
+         
         case"/": 
-         operando1 +operando2; break;
+         resultado = operando1 / operando2; 
+         alert(resultado);
+         break;
         case"-":
-             operando1 +operando2; break;
+             resultado = operando1 - operando2; 
+             alert(resultado);
+             break;
         case"*":
-             operando1 +operando2; break;
+             resultado = operando1 * operando2; 
+             alert(resultado);
+             break;
         case"FIN":  
-            operando1 +operando2; break;
-
-
-
-
+            alert("Finalizado"); break;
     }
 
 }
 
+
+function esEntero(numero) {
+
+    if (parseInt(numero)) {
+        return numero;
+    }
+    else {
+
+        while(!parseInt(numero)){
+            numero = parseInt(prompt("Eso no era un número prueba otra vez"));
+        }
+        return numero;
+    }
+}
+
+
+
+var operando1 = parseInt(prompt("Inserta un número"));
+ operando1 = esEntero(operando1);
+
+var operando2 = parseInt(prompt("Inserta otro número"));
+operando2 = esEntero(operando2);
+
+hacerOperacion( validarOperador(), operando1, operando2 );
 
