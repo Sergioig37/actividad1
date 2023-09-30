83b21e6cda0a5c13d2cb2a45e9320eb6976733c1
Sergioig37 <sergioig37@educastur.es>
2023-09-30 10:44:57 +0200

    Ejrcicio 5 acabado

--- ejercicio5/js/ejercicio5.js
+++ ejercicio5/js/ejercicio5.js
@@ -1,30 +1,65 @@
+
+
+function existeLogin(login) {
+
+    var loginValido = false;
+
+    for (var i = 0; i < usuarios.length; i++) {
+        if (login === usuarios[i]) {
+            loginValido = true;
+            break;
+        }
+    }
+    return loginValido;
+}
+
+function passwordCorrecto(password, login) {
+
+    var passwdValido = false;
+    var posicionLogin;
+
+    for (var i = 0; i < usuarios.length; i++) {
+        if (login === usuarios[i]) {
+            posicionLogin = i;
+            break;
+        }
+    }
+
+    if (passwords[posicionLogin] == password) {
+        passwdValido = true;
+    }
+
+    return passwdValido;
+}
+
+
 const usuarios = ["pepe", "paco", "juan", "benito", "manuel"];
 const passwords = ["12345", "23456", "09876", "67890", "666666"];
 
 
 
-function existeLogin(usuario){
+var numeroIntentos = 0;
+
+while (numeroIntentos <= 5) {
+   
+    var login = prompt("Nombre de usuario");
+
+    var password = prompt("Contraseña");
+
+
+    if (existeLogin(login) == false) {
+        alert("El nombre de usuario no existe");
+    }
+    else if (passwordCorrecto(password, login) == false) {
+        alert("El nombre de usuario no coincide con la contraseña");
+    }
+    else {
+        alert("El nombre de usuario y la contraseña son correctos");
+        break;
+    }
+    numeroIntentos++;
+    if(numeroIntentos==5){
+        alert("Has agotado todos tus intentos");
+    }
     
-    var loginValido  = false;
-
-    for(var i=0; i<usuarios.length; i++){
-
-        if(usuario===usuarios[i]){
-            loginValido = true;
-            break;
-        }
-    }   
-    if(loginValido == false){
-        while(loginValido==false){
-            usuario = prompt("Este usuario no existe, pruebe de nuevo");
-            for(var i=0; i<usuarios.length; i++){
-                if(usuario===usuarios[i]){
-                    loginValido = true;
-                    break;
-                }
-            }
-    }
-
-    }
-    return loginValido;
 }
